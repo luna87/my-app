387093b72029d0124a40baae3b5b965834259cfa
mona <mo.na.di.87.de@gmail.com>
2023-05-16 18:50:11 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Weather from "./Weather";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>weather app</h1>
+        <Weather city="new york" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
